940d567d368e8dec8f43b7ec96593ed88c8e084c
mahtias <kadjomathias1994@gmail.com>
2023-01-08 12:43:25 +0000

    fitrt depot

---
+++ main.js
@@ -0,0 +1,17 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1 Remove Banana from the array.
+fruits.shift()
+// 2 Sort the array in alphabetical order.
+fruits.sort();
+
+// 3 Add “Kiwi” to the end of the array.
+fruits.push("Kiwi")
+// 4 Remove “Apples” from the array. Don’t use the same method as in part 1.
+
+fruits.shift()
+
+// 5 Sort the array in reverse order. (Not alphabetical, but reverse the current Array
+
+fruits.reverse();
+console.log(fruits)
